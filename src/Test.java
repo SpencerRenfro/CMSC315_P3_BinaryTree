5c40db2f87ab490b337f42801556887e59995a92
SpencerRenfro <spencer.renfro13@gmail.com>
2025-04-18 16:04:28 -0600

    adds working print of binary map(but inversed)

--- src/Test.java
+++ src/Test.java
@@ -10,6 +10,7 @@ public class Test {
         public boolean isLeaf;
         public boolean isRoot;
         public boolean isBalanced;
+        public String binaryTreeString;
 
         public Node(int level, int value, int asteriskCount) {
             this.level = level;
@@ -36,6 +37,7 @@ public class Test {
             }
 
             isBalanced = hasLeftChild && hasRightChild;
+
         }
 
         @Override
@@ -70,6 +72,8 @@ public class Test {
         ArrayList<Node> nodeList = new ArrayList<>();
         Stack<ParseFrame> stack = new Stack<>();
         StringBuilder numberBuffer = new StringBuilder();
+        StringBuilder treeMapString = new StringBuilder();
+        String treeMapStringFinal = null;
         int level = 0;
 
         for (int i = 0; i < userInput.length(); i++) {
@@ -85,8 +89,14 @@ public class Test {
                 case ')' -> {
                     ParseFrame frame = stack.pop();
                     if (frame.value != null) {
+                        int tempLevel = frame.level;
                         Node node = new Node(frame.level, frame.value, frame.asteriskCount);
                         nodeList.add(node);
+                        while(tempLevel != 1) {
+                            treeMapString.append("\t");
+                            tempLevel--;
+                        }
+                        treeMapString.append(node.value).append("\n");
                         System.out.println(frame.asteriskCount + " asterisks for value " + frame.value);
                     }
                     level--;
@@ -113,5 +123,14 @@ public class Test {
 
         System.out.println("\nFinal Nodes:");
         nodeList.forEach(System.out::println);
+        System.out.println("\nBinary Tree String:");
+        StringBuilder tempBinaryTreeString = new StringBuilder();
+
+        treeMapStringFinal = treeMapString.toString();
+        System.out.println(treeMapStringFinal);
     }
 }
+
+
+
+/* Issue that arose was using stack to make the string, this made an inverse layout*/
